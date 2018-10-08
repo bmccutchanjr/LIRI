783a03a508e479c574db17bf1fa7f66b0d1d56a1
Bill McCutchan <bmccutchanjr@gmail.com>
2018-10-07 23:01:32 -0400

    omdb search functionality

--- OMDB.js
+++ OMDB.js
@@ -1,15 +1,51 @@
 var request = require("request");
+const log = require("./log.js");                           // log file module
 
-var search = function ()
-{   var url = "https://www.omdbapi.com/?s=Saw&apikey=78d19ea8";
+function printCast (cast)
+{   // The cast list is a comma delimited string
+
+    var actors = cast.split (",");
+
+    for (var i=0; i<actors.length; i++)
+    {   log.output ("     " + actors[i].trim());
+    }
+}
+
+function printRating (ratings)
+{
+    for (var i=0; i<ratings.length; i++)
+    {   log.output ("     " + ratings[i].Source  + ": " + ratings[i].Value);
+    }
+}
+
+function formatMovie (movie)
+{   // This data doesn't seem to apply to a single line of text as much as the Bands In Town search, so
+    // output several lines to make the results a little more readable. That also means this function will
+    // output data, and not return it to search()
+    
+    log.output ("==============================");
+    log.output (movie.Title);
+    log.output (movie.Plot);
+    log.output ("starring: ");
+    printCast (movie.Actors);
+    log.output ("rating: ");
+    printRating (movie.Ratings);
+    log.output ("released in: " + movie.Country + " in " + movie.Year);
+    log.output (movie.Language);
+}
+
+var search = function (...params)
+{   // Search OMDB for data on the indicated movie.  Searches are by title.
+
+    var url = "https://www.omdbapi.com/?t=Harold and&incTomatoes&apikey=78d19ea8";
     request(url, function(error, response, body)
     {   if(!error && response.statusCode == 200)
-        {   
-    console.log(JSON.parse(body));
+        {   // No errors and we have data...
+
+            formatMovie (JSON.parse(body));
         }
         else console.log (error);
     });
-//     console.log ("omdb.search ()");
 }
 
 module.exports.search = search;
